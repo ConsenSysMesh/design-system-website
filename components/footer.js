728a67d4d4e9630c5abc432a91168f1da9c2bf44
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-09-14 13:00:33 -0600

    add footer component

---
+++ components/footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Footer = ({}) => (
+  <div>
+    <ul>
+      <li>
+        <a href="#!">Github</a>
+      </li>
+      <li>
+        <a href="#!">Storybook/Examples</a>
+      </li>
+      <li>
+        <a href="#!">Community</a>
+      </li>
+      <li>
+        <a href="#!">Roadmap</a>
+      </li>
+      <li>
+        <a href="#!">Team</a>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Footer;
