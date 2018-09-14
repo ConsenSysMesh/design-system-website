6933bbc4885f91504a71df84c7d0777961473a5a
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-09-14 13:00:39 -0600

    add header component

---
+++ components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = ({}) => (
+  <header>
+    <h1>
+      "literate-sniffle"
+    </h1>
+    <h1>The most important heading on this page</h1>
+    <h2>With some supplementary information</h2>
+    <button>Get Started</button>
+  </header>
+);
+
+export default Header;
