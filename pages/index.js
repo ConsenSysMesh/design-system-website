b59db19cfd320f33be9153ef265850b765399a98
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-09-14 13:01:06 -0600

    tentatively add content

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,42 @@ import Link from 'next/link'
 import Head from '../components/head'
 import Nav from '../components/nav'
 
+import Footer from '../components/footer'
+import Header from '../components/header'
+
 import styled from 'styled-components'
 
-const Bold = styled.h1`
-  font-weight: bolder;
-  font-size: 2rem;
+const Masthead = styled.section`
+  position: relative;
+  height: 80vh;
+  width: 100%;
+
+  display: flex;
+  flex-flow: row;
+  align-items: center;
+  justify-content: center;
 `
 
 const Home = () => (
   <div>
     <Head title="Home" />
-    <Bold>Make it easier to create dApps with outstanding UX.</Bold>
+    <Nav/>
+
+    <main role="main">
+      <Masthead>
+        {/* masthead section */}
+        <Header/>
+      </Masthead>
+      <section>
+        {/* body section */}
+        <h1>Motivation</h1>
+        <p>Make it easier to create dApps with outstanding UX.</p>
+        <h1>Getting Started</h1>
+        <h1>Installation</h1>
+        <h1>Usage</h1>
+      </section>
+    </main>
+    <Footer/>
   </div>
 )
 
