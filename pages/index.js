7476964b88a531b2bbeafa7cf6e5c91c7a13ca52
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-09-14 12:21:48 -0600

    test styled

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,17 @@ import Link from 'next/link'
 import Head from '../components/head'
 import Nav from '../components/nav'
 
+import styled from 'styled-components'
+
+const Bold = styled.h1`
+  font-weight: bolder;
+  font-size: 2rem;
+`
+
 const Home = () => (
   <div>
     <Head title="Home" />
-    Make it easier to create dApps with outstanding UX.
+    <Bold>Make it easier to create dApps with outstanding UX.</Bold>
   </div>
 )
 
