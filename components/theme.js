555386d6bd876612671add7064aaf39dbbcb9e70
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-09-18 14:58:10 -0600

    add theme file

---
+++ components/theme.js
@@ -0,0 +1,17 @@
+const theme = {
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+  fontSizes: [12, 14, 16, 20, 24, 36, 48, 80, 96],
+  // fontWeights: [100, 200, 300, 400, 500],
+  fonts: {
+    serif: 'athelas, georgia, times, serif',
+    sansSerif:
+      '-apple-system, BlinkMacSystemFont, "avenir next", avenir, "helvetica neue", helvetica, ubuntu, roboto, noto, "segoe ui", arial, sans-serif',
+  },
+  colors: {
+    primary: 'purple',
+    black: '#000',
+    white: '#FFF'
+  }
+}
+
+export default theme
