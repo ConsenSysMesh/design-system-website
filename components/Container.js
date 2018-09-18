7b781f5a5efeb84e5ee70df63d66dae421cf2606
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-09-18 17:43:57 -0600

    add container component

---
+++ components/Container.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  position: relative;
+  width: 100%;
+  max-width: 1024px;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 0 1rem;
+`
+
+export default Container
