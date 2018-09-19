d34cd2b6c1ac86fba64f82ce2a051ae44e54b348
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-09-19 17:08:18 -0600

    use prism-react-renderer instead of prismjs

--- components/CodeBlock.js
+++ components/CodeBlock.js
@@ -1,31 +1,27 @@
 import React, { PureComponent } from 'react'
 
-import Prism from 'prismjs'
+// import Prism from 'prismjs'
+import Highlight, { defaultProps } from "prism-react-renderer";
 import style from '../static/prism-ghcolors.css'
+
 
 class CodeBlock extends PureComponent {
 
-  componentDidMount() {
-    this._hightlight()
-  }
-
-  componentDidUpdate() {
-    this._hightlight()
-  }
-
-  _hightlight() {
-    Prism.highlightElement(this._domNode, false)
-  }
-
-  _handleRef = (node) => {
-    this._domNode = node
-  }
-
   render() {
     return (
-      <pre>
-        <code ref={this._handleRef} className='language-javascript'>{this.props.children}</code>
-      </pre>
+      <Highlight {...defaultProps} code={this.props.code} language='jsx' theme={undefined}>
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className} style={style}>
+            {tokens.map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
     )
   }
 
