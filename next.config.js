0ec330f3456b3153887273f88f1ed7131a3114c2
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-09-19 14:37:26 -0600

    add next-css and upgrade next

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-module.exports = {
+const withCSS = require('@zeit/next-css')
+module.exports = withCSS({
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty'
-    }
-
+    config.node = {fs: 'empty'}
     return config
   }
-}
+})
