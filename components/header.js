db8f1fe3af3bde32fee16946d2228d33551fba16
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-09-18 18:06:41 -0600

    debug builds failing

--- components/header.js
+++ components/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from '../components/Button.js'
+import Button from '../components/button.js'
 import Heading from '../components/Heading'
 
 const Header = ({}) => (
