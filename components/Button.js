bf42fe1d800950f2fe3cc4e0bc57095e1169c178
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-09-20 13:58:31 -0600

    add more button styles

--- components/Button.js
+++ components/Button.js
@@ -2,32 +2,55 @@ import React from 'react';
 import styled from 'styled-components'
 
 const Button = styled.button`
-  font-size: 14px;
-  ${'' /* margin: 1rem 0; */}
-  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  padding: 0 1.5rem;
   border-radius: 4px;
   cursor: pointer;
+  text-decoration: none;
+  white-space: nowrap;
+  height: 48px;
+  ${'' /* line-height: 48px; */}
+  font-weight: bolder;
+
+  box-shadow:
+    0 4px 6px rgba(50,50,93,.11),
+    0 1px 3px rgba(0,0,0,.08)
+  ;
+  transition: all .15s ease;
+
+  &:not(:last-of-type) {
+    margin-right: 1rem;
+  }
 
   /* Color the border and text with theme.main */
+  border: 1px solid transparent;
   color: white;
+  border-color: ${props => props.theme.colors.primary};
   background: ${props => props.theme.colors.primary};
-  border: 2px solid ${props => props.theme.colors.primary};
 
   &:hover {
-    box-shadow: 0 1px 2px 0 blue, 0 1px 3px 1px blue);
+    box-shadow:
+      0 7px 14px rgba(50,50,93,.1),
+      0 3px 6px rgba(0,0,0,.08)
+    ;
+    transform: translateY(-1px);
   }
 `;
 
 Button.outline = styled(Button)`
   color: ${props => props.theme.colors.primary};
+  border-color: ${props => props.theme.colors.primary};
   background: transparent;
-  border: 2px solid ${props => props.theme.colors.primary};
+  &:hover {
+    background-color: rgba(32,33,36,0.059);
+  }
 `
 
 Button.text = styled(Button)`
   color: ${props => props.theme.colors.primary};
+  border-color: transparent;
   background: transparent;
-  border: 2px solid transparent;
+  box-shadow: none;
 `
 
 export default Button;
