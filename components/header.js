431d20c198d490c90db6a099025389653817f699
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-09-20 15:28:53 -0600

    reformat text a bit

--- components/header.js
+++ components/header.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Button from './Button'
 import Heading from './Heading'
+import Text from './Text'
 
 const Header = ({}) => (
   <header>
-    <Heading.h1>
-      "literate-sniffle"
-    </Heading.h1>
-    <Heading.h1>The most important heading on this page</Heading.h1>
-    <Heading.h2>With some supplementary information</Heading.h2>
+    <Heading.h1>Literate-sniffle</Heading.h1>
+    <Heading>The most important heading on this page</Heading>
+    <Text.p>With some supplementary information</Text.p>
+
     <Button>Get Started</Button>
     <Button.outline>outline button</Button.outline>
     <Button.text>text button</Button.text>
