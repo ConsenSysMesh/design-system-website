8cc64887b6c10d4dc4cf78aa36b182376a0e6a83
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-09-20 15:26:40 -0600

    style buttons a bit more

--- components/Button.js
+++ components/Button.js
@@ -1,55 +1,63 @@
-import React from 'react';
+import React from 'react'
 import styled from 'styled-components'
 
-const Button = styled.button`
+import { tint } from 'polished'
+
+const Button = styled.button.attrs({
+  fgcolor: props => props.theme.colors.white,
+  bgcolor: props => props.theme.colors.primary,
+  hovercolor: props => tint(0.9, props.theme.colors.primary)
+})`
+  font: inherit;
+  color: inherit;
+  border: none;
+  cursor: pointer;
+  white-space: nowrap;
+  text-decoration: none;
+
   font-size: 1rem;
+  font-weight: bolder;
+  ${'' /* line-height: 48px; */}
+  height: 48px;
   padding: 0 1.5rem;
   border-radius: 4px;
-  cursor: pointer;
-  text-decoration: none;
-  white-space: nowrap;
-  height: 48px;
-  ${'' /* line-height: 48px; */}
-  font-weight: bolder;
-
   box-shadow:
     0 4px 6px rgba(50,50,93,.11),
     0 1px 3px rgba(0,0,0,.08)
   ;
   transition: all .15s ease;
-
-  &:not(:last-of-type) {
-    margin-right: 1rem;
-  }
-
   /* Color the border and text with theme.main */
-  border: 1px solid transparent;
-  color: white;
-  border-color: ${props => props.theme.colors.primary};
-  background: ${props => props.theme.colors.primary};
+  color: ${props => props.fgcolor};
+  background-color: ${props => props.bgcolor};
 
   &:hover {
+    background-color: ${ props => props.hovercolor };
+    transform: translateY(-1px);
     box-shadow:
       0 7px 14px rgba(50,50,93,.1),
       0 3px 6px rgba(0,0,0,.08)
     ;
-    transform: translateY(-1px);
+  }
+  &:not(:last-of-type) {
+    margin-right: 1rem;
   }
 `;
 
-Button.outline = styled(Button)`
-  color: ${props => props.theme.colors.primary};
-  border-color: ${props => props.theme.colors.primary};
-  background: transparent;
+Button.outline = styled(Button).attrs({
+  fgcolor: props => props.theme.colors.primary,
+  bgcolor: props => props.theme.colors.white,
+  hovercolor: props => tint(0.1, props.theme.colors.primary)
+})`
+  color: ${props => props.fgcolor};
+  background-color: transparent;
+  border: 1px solid ${props => props.fgcolor};
   &:hover {
-    background-color: rgba(32,33,36,0.059);
+    background-color: ${props => props.hovercolor}
   }
 `
 
-Button.text = styled(Button)`
-  color: ${props => props.theme.colors.primary};
-  border-color: transparent;
-  background: transparent;
+Button.text = styled(Button.outline)`
+  border: none;
   box-shadow: none;
 `
 
