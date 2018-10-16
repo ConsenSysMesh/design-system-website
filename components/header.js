9d9c7b28a324b10632d74008b48badae83c843a3
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-10-16 15:39:18 -0600

    update header contents

--- components/header.js
+++ components/header.js
@@ -6,8 +6,8 @@ import Button from './Button'
 
 const Header = ({}) => (
   <header>
-    <Heading.h1 fontSize={[5, 6]}>"literate-sniffle" Design System</Heading.h1>
-    <Text.p>React component library for building dApps.</Text.p>
+    <Heading.h1 fontSize={[5, 6]}>Sniffle Design System</Heading.h1>
+    <Text.p fontSize={3} mb={5}>Adaptable components and design standards for decentralized applications.</Text.p>
 
     <Button>Get Started</Button>
     <Button.outline>Documentation</Button.outline>
