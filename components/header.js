34f3cc3a0270c88dc1f153bb311853dc1ba76b97
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-10-17 14:54:54 -0600

    add padding to increase text legibility on certain viewport sizes

--- components/header.js
+++ components/header.js
@@ -6,8 +6,8 @@ import Button from './Button'
 
 const Header = ({}) => (
   <header>
-    <Heading.h1 fontSize={[5, 7]}>Rimble Design System</Heading.h1>
-    <Text.p fontSize={2} mt={3} mb={5}>Adaptable components and design standards for decentralized applications.</Text.p>
+    <Heading.h1 fontSize={[5, 7]} pr={[3, 6]}>Rimble Design System</Heading.h1>
+    <Text.p fontSize={2} mt={3} mb={5} pr={[3, 6]}>Adaptable components and design standards for decentralized applications.</Text.p>
 
     <Button width={['100%', 'auto']} mr={[0, 3]} mb={[3, 0]}>Get Started</Button>
     <Button.outline width={['100%', 'auto']}>Documentation</Button.outline>
