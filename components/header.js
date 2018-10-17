94093f653cdf0f189caa1999e1d72d7e377b4aa8
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-10-17 13:31:25 -0600

    responsive styles for header CTAs

--- components/header.js
+++ components/header.js
@@ -9,8 +9,8 @@ const Header = ({}) => (
     <Heading.h1 fontSize={[5, 7]}>Rimble Design System</Heading.h1>
     <Text.p fontSize={2} mt={3} mb={5}>Adaptable components and design standards for decentralized applications.</Text.p>
 
-    <Button>Get Started</Button>
-    <Button.outline>Documentation</Button.outline>
+    <Button width={['100%', 'auto']} mr={[0, 3]} mb={[3, 0]}>Get Started</Button>
+    <Button.outline width={['100%', 'auto']}>Documentation</Button.outline>
   </header>
 );
 
