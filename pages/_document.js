64cf033e634520dbfddcd4f34d77b3b55e94e501
Gustavo Esquinca <g.esq.ca@gmail.com>
2018-10-29 09:19:17 +0100

    update tag manager code

--- pages/_document.js
+++ pages/_document.js
@@ -13,22 +13,18 @@ export default class MyDocument extends Document {
     return (
       <html lang='en'>
         <Head>
-          <!-- Google Tag Manager -->
-          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-54DHXVC');</script>
-          <!-- End Google Tag Manager -->
+          <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-54DHXVC');`}} />
 
           <title>My page</title>
           {this.props.styleTags}
         </Head>
         <body>
-          <!-- Google Tag Manager (noscript) -->
-          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-54DHXVC"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-          <!-- End Google Tag Manager (noscript) -->
+          <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-54DHXVC" height="0" width="0" style="display:none;visibility:hidden"></iframe>`}} />
+
           <Main />
           <NextScript />
         </body>
